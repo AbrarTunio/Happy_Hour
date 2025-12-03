f13a321510d20b2fcd6273f5a6651f0f04997ad9
Abrar Hussain <65803677+AbrarTunio@users.noreply.github.com>
2025-12-03 20:59:17 +0500

    Update and rename TimeSheet.php to Timesheet.php

--- app/Models/Timesheet.php
+++ app/Models/Timesheet.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class TimeSheet extends Model
+class Timesheet extends Model
 {
     //
     use HasFactory;
